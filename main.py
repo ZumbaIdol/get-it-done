fe21e7247a9762dc8385f2f2debc84b3abe40609
David A. Foote <david.a.foote@gmail.com>
2017-10-08 00:09:16 -0500

    more functionality but still can't register

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name
         self.completed = False
+        self.owner = owner
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    tasks = db.relationship('Task', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
@@ -88,7 +91,8 @@ def index():
     
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)
+        owner = User.query.filter_by(email=session['email']).first()
+        new_task = Task(task_name, owner)
         db.session.add(new_task)
         db.session.commit()
 
